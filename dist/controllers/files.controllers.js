0bdbdc03d0eb51823fd13d974d34c00ece914e22
chainuocngot <anhvu.dev.103@gmail.com>
2024-08-07 20:16:28 +0700

    update ext

--- dist/controllers/files.controllers.js
+++ dist/controllers/files.controllers.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.downloadExtensionController = void 0;
 const path_1 = __importDefault(require("path"));
 const downloadExtensionController = async (req, res) => {
-    const file = path_1.default.resolve('src/assets/dist.zip');
+    const file = path_1.default.resolve('src/assets/c_Lab_207.zip');
     return res.download(file);
 };
 exports.downloadExtensionController = downloadExtensionController;
